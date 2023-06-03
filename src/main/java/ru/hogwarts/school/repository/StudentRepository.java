649963226ee4b1578cb7dfb024bc87f398f55e7d
Azat <aelchikushuulu@gmail.com>
2023-06-03 15:17:37 +0600

    Initial commit

---
+++ src/main/java/ru/hogwarts/school/repository/StudentRepository.java
@@ -0,0 +1,16 @@
+package ru.hogwarts.school.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.hogwarts.school.entity.Student;
+
+import java.util.List;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    List<Student> findAllByAge(int age);
+
+    List<Student> findAllByAgeBetween(int minAge, int maxAge);
+
+    List<Student> findAllByFaculty_Id(Long facultyId);
+
+}
